43f84b2d652de15a11427b11c0b02d2082fdcdf2
Girl DoCode <girldocode@gmail.com>
2024-08-24 14:59:10 +0500

    feat: develop design system

---
+++ src/~type.d.ts
@@ -0,0 +1,24 @@
+export type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+export type Color =
+  | "blue-gray-100"
+  | "blue-gray-300"
+  | "blue-gray-400"
+  | "blue-gray-600"
+  | "blue-gray-900"
+  | "white"
+  | "yellow-400"
+  | "blue-600"
+  | "blue-100"
+  | "blue-800"
+  | "blue-gray-600"
+  | "purple-100"
+  | "purple-800"
+  | "green-100"
+  | "green-800"
+  | "orange-400"
+  | "orange-600"
+  | "blue-gray-200"
+  | "blue-gray-700"
+  | "Purple-900"
+  | "Purple-600";
